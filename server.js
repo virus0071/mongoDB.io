b0277edb03765481d62b463929649fb57090b26d
Nan Wu <virus00719@gmail.com>
2018-09-13 16:49:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://heroku_2tdxqznz:iP0GmWwoeD8u0Rnj_9yoabtIwlyWgClm@ds255332.mlab.com:55332/heroku_2tdxqznz");
+mongoose.connect("mongodb://heroku_2tdxqznz:789789Love##@ds255332.mlab.com:55332/heroku_2tdxqznz");
 var db = mongoose.connection;
 
 db.on("error", function(error) {
